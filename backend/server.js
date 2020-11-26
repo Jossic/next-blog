2064d3c741b92b70086cd3468fc44146e2b962ef
Jossic <jossic.lapierre@gmail.com>
2020-11-26 13:38:16 +0100

    next-blog - 11 - Separating routes

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,37 @@
-const express = require('express')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const mongoose = require('mongoose')
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
+const blogRoutes = require("./routes/blogRoutes");
 
-const app = express()
+const app = express();
 
-mongoose.connect(process.env.DATABASE_CLOUD, {
+mongoose
+  .connect(process.env.DATABASE_CLOUD, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
-}).then( () => {
-    console.log('Database Connected')
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Database Connected");
+  });
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 }
-)
 
-app.use(morgan('dev'))
-app.use(bodyParser.json())
-app.use(cookieParser())
+app.use("/api", blogRoutes);
 
-if (process.env.NODE_ENV ==='development') {
-        app.use(cors({origin: `${process.env.CLIENT_URL}`}))
-}
-
-app.get('/api', (req, res) => {
-    res.json({time: Date().toString()})
-})
-
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)   
-})
+  console.log(`Server is running on port ${port}`);
+});
