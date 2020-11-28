f4ffd8bb69781d545577c7ffe89f4ca08e73d09d
Jossic <jossic.lapierre@gmail.com>
2020-11-28 21:07:58 +0100

    next-blog - 35 - Signin

---
+++ frontend/components/auth/signinComponent.js
@@ -0,0 +1,83 @@
+import { useState } from 'react';
+import { signin } from '../../actions/authAction';
+import Router from 'next/router';
+
+const SigninComponent = () => {
+	const [values, setvalues] = useState({
+		email: '',
+		password: '',
+		error: '',
+		loading: false,
+		message: '',
+		showForm: true,
+	});
+
+	const { email, password, error, loading, message, showForm } = values;
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		setvalues({ ...values, loading: true, error: false });
+		const user = { email, password };
+
+		signin(user).then((data) => {
+			if (data.error) {
+				setvalues({ ...values, error: data.error, loading: false });
+			} else {
+				Router.push('/');
+			}
+		});
+	};
+
+	const handleChange = (val) => (e) => {
+		setvalues({ ...values, error: false, [val]: e.target.value });
+	};
+
+	const showLoading = () =>
+		loading ? <div className='alert alert-info'>Loading...</div> : '';
+	const showError = () =>
+		error ? <div className='alert alert-danger'>{error}</div> : '';
+	const showMessage = () =>
+		message ? <div className='alert alert-info'>{message}</div> : '';
+
+	const signinForm = () => {
+		return (
+			<form onSubmit={handleSubmit}>
+				<div className='form-group'>
+					<input
+						value={email}
+						onChange={handleChange('email')}
+						type='email'
+						className='form-control'
+						placeholder='Entrez votre email'
+					/>
+				</div>
+
+				<div className='form-group'>
+					<input
+						value={password}
+						onChange={handleChange('password')}
+						type='password'
+						className='form-control'
+						placeholder='Entrez votre mot de passe'
+					/>
+				</div>
+
+				<div>
+					<button className='btn btn-primary'>Se connecter</button>
+				</div>
+			</form>
+		);
+	};
+
+	return (
+		<>
+			{showError()}
+			{showLoading()}
+			{showMessage()}
+			{showForm && signinForm()}
+		</>
+	);
+};
+
+export default SigninComponent;
--- frontend/pages/signin.js
+++ frontend/pages/signin.js
@@ -1,9 +1,14 @@
 import Layout from '../components/Layout';
 import Link from 'next/link';
+import SigninComponent from '../components/auth/SigninComponent';
+
 const Signin = () => {
 	return (
 		<Layout>
-			<h2>Signin</h2>
+			<h2 className='text-center pt-4 pb-4'>Se connecter</h2>
+			<div className='col-md-6 offset-md-3'>
+				<SigninComponent />
+			</div>
 			<Link href='/'>
 				<a>Accueil</a>
 			</Link>
