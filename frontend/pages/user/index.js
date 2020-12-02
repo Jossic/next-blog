8d14272d26197754c33493d25be5d276ababd4b5
Jossic <jossic.lapierre@gmail.com>
2020-12-02 10:23:00 +0100

    next-blog - 43 - Protecting Routes for auth & Admin

--- frontend/pages/user/index.js
+++ frontend/pages/user/index.js
@@ -1,10 +1,13 @@
 import Layout from '../../components/Layout';
+import Private from '../../components/auth/Private';
 import Link from 'next/link';
 
 const UserIndex = () => {
 	return (
 		<Layout>
-			<h2>Dashboard utilisateur</h2>
+			<Private>
+				<h2>Dashboard utilisateur</h2>
+			</Private>
 		</Layout>
 	);
 };
