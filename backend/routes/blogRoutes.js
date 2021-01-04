de7ae49c390a20c11126cc846fc67bb2e1bdd038
Jossic <jossic.lapierre@gmail.com>
2021-01-04 15:08:42 +0100

    next-blog - 79 - List All Blogs

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const { create } = require('../controllers/blogController');
+const {
+	create,
+	list,
+	listAllBlogsCategoriesTags,
+	read,
+	remove,
+	update,
+} = require('../controllers/blogController');
 const {
 	requireSignin,
 	authMiddleware,
 } = require('../controllers/authController');
 
 router.post('/blog', requireSignin, authMiddleware, create);
+router.get('/blogs', list);
+router.post('/blogs-categories-tags', listAllBlogsCategoriesTags);
+router.get('/blog/:slug', read);
+router.delete('/blog/:slug', requireSignin, authMiddleware, remove);
+router.put('/blog/:slug', requireSignin, authMiddleware, update);
 
 module.exports = router;
