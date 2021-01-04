40540fc39198bcbbe4dd46e0913558d11375c976
Jossic <jossic.lapierre@gmail.com>
2021-01-04 13:18:20 +0100

    next-blog - 72 - Blog create Form

--- frontend/components/crud/BlogCreate.js
+++ frontend/components/crud/BlogCreate.js
@@ -11,12 +11,69 @@ const ReactQuilll = dynamic(() => import('react-quill'), { ssr: false });
 import '../../node_modules/react-quill/dist/quill.snow.css';
 
 const BlogCreate = ({ router }) => {
-	return (
-		<div>
-			<h2>Formulaire</h2>
-			{JSON.stringify(router)}
-		</div>
-	);
+	const [body, setBody] = useState({});
+	const [values, setValues] = useState({
+		error: '',
+		sizeError: '',
+		success: '',
+		formData: '',
+		title: '',
+		hidePublishButton: false,
+	});
+
+	const {
+		error,
+		sizeError,
+		success,
+		formData,
+		title,
+		hidePublishButton,
+	} = values;
+
+	const publishBlog = (e) => {
+		e.preventDefault();
+		console.log('ok pour publication');
+	};
+
+	const handleChange = (name) => (e) => {
+		console.log(e.target.value);
+	};
+
+	const handleBody = (e) => {
+		console.log(e);
+	};
+
+	const createBlogForm = () => {
+		return (
+			<form onSubmit={publishBlog}>
+				<div className='form-group'>
+					<label htmlFor='' className='text-muted'>
+						Titre
+					</label>
+					<input
+						type='text'
+						className='form-control'
+						value={title}
+						onChange={handleChange('title')}
+					/>
+				</div>
+				<div className='form-group'>
+					<ReactQuilll
+						value={body}
+						placeholder='Votre article'
+						onChange={handleBody}
+					/>
+				</div>
+
+				<div>
+					<button type='submit' className='btn btn-primary'>
+						Publier
+					</button>
+				</div>
+			</form>
+		);
+	};
+	return <div>{createBlogForm()}</div>;
 };
 
 export default withRouter(BlogCreate);
