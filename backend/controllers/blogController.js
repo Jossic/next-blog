6c7d6a4ba8b1e31c838d382796625fd14ca5e5f4
Jossic <jossic.lapierre@gmail.com>
2021-01-04 15:20:55 +0100

    next-blog - 80 - List All Blogs, Categories & Tags

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -120,7 +120,56 @@ exports.list = (req, res) => {
 			res.json(data);
 		});
 };
-exports.listAllBlogsCategoriesTags = (req, res) => {};
+
+exports.listAllBlogsCategoriesTags = (req, res) => {
+	let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+	let skip = req.body.skip ? parseInt(req.body.skip) : 0;
+
+	let blogs;
+	let categories;
+	let tags;
+
+	Blog.find({})
+		.populate('categories', '_id name slug')
+		.populate('tags', '_id name slug')
+		.populate('postedBy', '_id name username profile')
+		.sort({ createdAt: -1 })
+		.skip(skip)
+		.limit(limit)
+		.select(
+			'_id title slug excerpt categories tags postedBy createdAt updatedAt'
+		)
+		.exec((err, data) => {
+			if (err) {
+				return res.json({
+					error: errorHandler(err),
+				});
+			}
+			blogs = data;
+
+			Category.find({}).exec((err, cat) => {
+				if (err) {
+					return res.json({
+						error: errorHandler(err),
+					});
+				}
+				categories = cat;
+				Tag.find({}).exec((err, tag) => {
+					if (err) {
+						return res.json({
+							error: errorHandler(err),
+						});
+					}
+					tags = tag;
+
+					res.json({ blogs, categories, tags, size: blogs.length });
+				});
+			});
+		});
+};
+
 exports.read = (req, res) => {};
+
 exports.remove = (req, res) => {};
+
 exports.update = (req, res) => {};
