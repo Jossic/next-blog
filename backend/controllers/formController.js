7db501b0ee7194f7c8c3fa27c05f2c6705d917d7
Jossic <jossic.lapierre@gmail.com>
2021-01-11 14:29:10 +0100

    next-blog - 137 - Contact Form Author - Backend

--- backend/controllers/formController.js
+++ backend/controllers/formController.js
@@ -20,3 +20,26 @@ exports.contactForm = (req, res) => {
 
 	sendEmailWithNodemailer(req, res, emailData);
 };
+
+exports.contactBlogAuthorForm = (req, res) => {
+	console.log(req.body);
+	const { authorEmail, name, email, message } = req.body;
+
+	let mailList = [authorEmail, process.env.NODE_MAILER_USER];
+
+	const emailData = {
+		from: process.env.NODE_MAILER_USER, // MAKE SURE THIS EMAIL IS YOUR GMAIL FOR WHICH YOU GENERATED APP PASSWORD
+		to: process.env.NODE_MAILER_USER, // WHO SHOULD BE RECEIVING THIS EMAIL? IT SHOULD BE YOUR GMAIL
+		subject: `${process.env.APP_NAME} | Formulaire de contact`,
+		text: `Blog Next-JS \n Venant de: ${name} \n email: ${email} \n Son message: ${message}`,
+		html: `
+        <h4>Email reçu du formulaire de contact:</h4>
+        <p>Nom: ${name}</p>
+        <p>Email: ${email}</p>
+        <p>Message: ${message}</p>
+        <hr />
+    `,
+	};
+
+	sendEmailWithNodemailer(req, res, emailData);
+};
